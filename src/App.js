e706819036961009967a81606b684a005fc518fe
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-10-01 10:36:46 +0330

    change app.js and add routes done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import { Route, Routes,Navigate } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-     
+     <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path='/' element={<Navigate to={'/signup'}/>}/>
+      </Routes>
     </div>
   );
 }
